dc1bcc38c8edd815a8b3082c7b6e038de71f99f8
Mike Griese <migrie@microsoft.com>
2025-09-15 12:54:28 -0500

    we're back in business boys

--- DeskBand11/MainWindow.xaml.cs
+++ DeskBand11/MainWindow.xaml.cs
@@ -1,3 +1,7 @@
+using Microsoft.UI.Windowing;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
 using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -10,9 +14,46 @@ namespace DeskBand11
     /// </summary>
     public sealed partial class MainWindow : WindowEx
     {
+        private readonly HWND _hwnd;
+
         public MainWindow()
         {
             InitializeComponent();
+            _hwnd = new HWND(WinRT.Interop.WindowNative.GetWindowHandle(this).ToInt32());
+            ExtendsContentIntoTitleBar = true;
+            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Collapsed;
+            MoveToTaskbar();
+        }
+
+        private void MoveToTaskbar()
+        {
+            HWND thisWindow = _hwnd;
+
+            HWND taskbarWindow = PInvoke.FindWindow("Shell_TrayWnd", null);
+            HWND reBarWindow = PInvoke.FindWindowEx(taskbarWindow, HWND.Null, "ReBarWindow32", null);
+
+            WINDOW_STYLE oldStyle = (WINDOW_STYLE)PInvoke.GetWindowLong(thisWindow, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+            WINDOW_STYLE oldStyleButNotPopup = oldStyle & (~WINDOW_STYLE.WS_POPUP);
+            WINDOW_STYLE nowAddChild = oldStyleButNotPopup | WINDOW_STYLE.WS_CHILD;
+
+            PInvoke.SetWindowLong(thisWindow, WINDOW_LONG_PTR_INDEX.GWL_STYLE, (int)nowAddChild);
+            PInvoke.SetParent(thisWindow, taskbarWindow);
+
+            RECT taskbarRect = new();
+            PInvoke.GetWindowRect(taskbarWindow, out taskbarRect);
+
+            RECT reBarRect = new();
+            PInvoke.GetWindowRect(reBarWindow, out reBarRect);
+
+            PInvoke.SetWindowPos(thisWindow,
+                         HWND.Null,
+                         taskbarRect.left,
+                         reBarRect.top - taskbarRect.top,
+                         taskbarRect.right - taskbarRect.left,
+                         reBarRect.bottom - reBarRect.top,
+                         0);
+
+            //HostControl.Width = Height;
         }
     }
 }
