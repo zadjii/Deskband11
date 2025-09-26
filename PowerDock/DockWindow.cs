3419798b832934cf6e1eb9581d4bb730405997bc
Mike Griese <migrie@microsoft.com>
2025-09-26 15:39:18 -0500

    dock it

--- PowerDock/DockWindow.cs
+++ PowerDock/DockWindow.cs
@@ -1,6 +1,13 @@
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.WindowsAndMessaging;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -14,10 +21,87 @@ namespace PowerDock
     {
         ObservableCollection<TaskbarApp> TaskbarItems { get; set; } = new ObservableCollection<TaskbarApp>();
 
+        private HWND _hwnd = HWND.Null;
+        private uint _callbackMessageId;
+
         public DockWindow()
         {
             InitializeComponent();
+
+            ExtendsContentIntoTitleBar = true;
+            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Collapsed;
+            AppWindow.IsShownInSwitchers = false;
+            this.Activated += MainWindow_Activated;
         }
+
+        private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            if (_hwnd == HWND.Null)
+            {
+                _hwnd = GetWindowHandle(this);
+                RegisterAppBar(_hwnd);
+            }
+        }
+        private HWND GetWindowHandle(Window window)
+        {
+            nint hwnd = WindowNative.GetWindowHandle(window);
+            return new HWND(hwnd);
+        }
+        private void RegisterAppBar(HWND hwnd)
+        {
+            _callbackMessageId = PInvoke.RegisterWindowMessage("AppBarMessage");
+
+            APPBARDATA abd = new()
+            {
+                cbSize = (uint)Marshal.SizeOf<APPBARDATA>(),
+                hWnd = hwnd,
+                uCallbackMessage = _callbackMessageId
+            };
+
+            // Register this window as an appbar
+            PInvoke.SHAppBarMessage(ABM_NEW, ref abd);
+
+            int height = 32; // height of your bar
+
+            int screenWidth = PInvoke.GetSystemMetrics(SYSTEM_METRICS_INDEX.SM_CXSCREEN);
+
+            abd.uEdge = ABE_TOP;
+            abd.rc.left = 0;
+            abd.rc.top = 0;
+            abd.rc.right = screenWidth;
+            abd.rc.bottom = height;
+
+            // Query and set position
+            PInvoke.SHAppBarMessage(ABM_QUERYPOS, ref abd);
+            PInvoke.SHAppBarMessage(ABM_SETPOS, ref abd);
+
+            // Move the actual window
+            PInvoke.MoveWindow(hwnd,
+                abd.rc.left,
+                abd.rc.top,
+                abd.rc.right - abd.rc.left,
+                abd.rc.bottom - abd.rc.top,
+                true);
+        }
+        private static readonly uint ABM_NEW = 0x0;
+        private static readonly uint ABM_REMOVE = 0x1;
+        private static readonly uint ABM_QUERYPOS = 0x2;
+        private static readonly uint ABM_SETPOS = 0x3;
+        private static readonly uint ABM_GETSTATE = 0x4;
+
+        private static readonly uint ABE_TOP = 0x1;
+        //protected override void OnClosed(WindowEventArgs args)
+        //{
+        //    // Unregister appbar when closed
+        //    APPBARDATA abd = new()
+        //    {
+        //        cbSize = (uint)Marshal.SizeOf<APPBARDATA>(),
+        //        hWnd = _hWnd
+        //    };
+        //    PInvoke.SHAppBarMessage(ABM_REMOVE, ref abd);
+
+        //    base.OnClosed(args);
+        //}
 
         [RelayCommand]
         void PressButton()
